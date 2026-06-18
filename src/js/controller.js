d3347e2a134365e3a32375090eda332f6cbd1988
naleilan <leilanschmitz@gmail.com>
2026-06-18 10:51:25 +0400

    added Api

--- src/js/controller.js
+++ src/js/controller.js
@@ -16,12 +16,26 @@ const timeout = function (s) {
 const showRecipe = async function () {
   try {
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      // 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc990',
     );
     const data = await res.json();
 
-    if (!res.ok) throw new Error(`${data.message} `);
-    console.log(res, data);
+    if (!res.ok) throw new Error(`${data.message} (${res.status}) `);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      image: recipe.image_url,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+      sourceUrl: recipe.source_url,
+    };
+
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }
